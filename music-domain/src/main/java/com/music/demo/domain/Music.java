4c3b11ca7abe2e7c7ef9a87e44fefdd12e471f67
songsongyl <2444616264@qq.com>
2025-06-23 10:16:34 +0800

    music

--- music-domain/src/main/java/com/music/demo/domain/Music.java
+++ music-domain/src/main/java/com/music/demo/domain/Music.java
@@ -1,4 +1,15 @@
 package com.music.demo.domain;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("music")
 public class Music {
 }
